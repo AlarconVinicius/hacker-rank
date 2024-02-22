9645af5b3a35469dd95d08a5fb4d6b4b1c825881
[allanprado] <[allanprado.it@gmail.com]>
2024-02-22 19:05:20 -0300

    feat(C)fix: add new resolution

--- C/plus_minus/resolution.c
+++ C/plus_minus/resolution.c
@@ -1,3 +1,29 @@
+void plusMinus(int arr_count, int* arr) {
+    float p;
+    float n;
+    float z;
+    int i;
+    
+    p = 0;
+    n = 0;
+    z = 0;
+    i = 0;
+    while (i < arr_count)
+    {
+        if (arr[i] < 0)
+            n++;
+        else if (arr[i] > 0)
+            p++;
+        else
+            z++;
+        i++;
+    }
+    n = n/arr_count;
+    p = p/arr_count;
+    z = z/arr_count;
+    printf("%0.6f \n%0.6f \n%0.6f", p, n, z);
+}
+
 void plusMinus(int arr_count, int* arr) 
 {
     int p;
